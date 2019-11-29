bab80e54edcd958cc346cde4262772bd3c14d2fe
Jan Molak <jan.molak@smartcodeltd.co.uk>
2019-11-29 18:19:39 +0000

    Use the new @serenity-js/console-reporter

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,4 +1,5 @@
 const
+    { ConsoleReporter } = require('@serenity-js/console-reporter'),
     { ArtifactArchiver } = require('@serenity-js/core'),
     { Photographer, TakePhotosOfInteractions } = require('@serenity-js/protractor'),
     { SerenityBDDReporter } = require('@serenity-js/serenity-bdd'),
@@ -25,6 +26,7 @@ exports.config = {
         runner: 'cucumber',
         crew: [
             ArtifactArchiver.storingArtifactsAt('./target/site/serenity'),
+            ConsoleReporter.forDarkTerminals(),
             Photographer.whoWill(TakePhotosOfInteractions),     // or Photographer.whoWill(TakePhotosOfFailures),
             new SerenityBDDReporter(),
         ]
