83364221578628242d9ea9aeb185eb9d1bf37b15
Jan Molak <1089173+jan-molak@users.noreply.github.com>
2024-11-20 00:07:56 +0000

    chore(deps): pinned Chrome and ChromeDriver versions

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,11 +1,23 @@
+const { computeExecutablePath } = require('@puppeteer/browsers')
+const { resolve } = require('path')
+
+// Chrome 129 is the last version that correctly supports Selenium 3
+// Chrome 130 and later require Selenium 4 for browser.executeScript to correctly resolve WebElement arguments
+const chromeVersion = '129';
+
+const binaries = {
+    chromedriver: computeExecutablePath({ browser: 'chromedriver', buildId: chromeVersion, cacheDir: '.' }),
+    chrome: computeExecutablePath({ browser: 'chrome', buildId: chromeVersion, cacheDir: '.' }),
+}
+
 exports.config = {
     baseUrl: 'https://juliemr.github.io/',
-
-    chromeDriver: require(`chromedriver/lib/chromedriver`).path,
 
     SELENIUM_PROMISE_MANAGER: false,
 
     directConnect: true,
+
+    chromeDriver: binaries.chromedriver,
 
     // https://github.com/angular/protractor/blob/master/docs/timeouts.md
     allScriptsTimeout: 110000,
@@ -61,6 +73,9 @@ exports.config = {
         },
 
         chromeOptions: {
+            w3c: false,
+            binary: binaries.chrome,
+            excludeSwitches: [ 'enable-automation' ],
             args: [
                 '--disable-web-security',
                 '--allow-file-access-from-files',
